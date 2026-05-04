5bf8b2bbb9f49f08e034d437e94b3b650de5edb2
Ilya Vorobev <via090105@gmail.com>
2026-05-04 20:46:55 +0300

    Fix all migration files with correct down_revision chain

--- alembic/versions/ba8e1d072d9e_add_type_column_to_transactions.py
+++ alembic/versions/ba8e1d072d9e_add_type_column_to_transactions.py
@@ -2,28 +2,22 @@
 
 Revision ID: ba8e1d072d9e
 Revises: a2feb255b408
-Create Date: 2026-05-02 21:10:42.948965
+Create Date: 2024-01-04 00:00:00.000000
 
 """
-from typing import Sequence, Union
-
 from alembic import op
 import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'ba8e1d072d9e'
-down_revision: Union[str, Sequence[str], None] = 'a2feb255b408'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision = 'ba8e1d072d9e'
+down_revision = 'a2feb255b408'  # ← Ссылается на третью миграцию
+branch_labels = None
+depends_on = None
 
 
 def upgrade() -> None:
-    # 1. Добавляем колонку с дефолтным значением 'expense'
     op.add_column('transactions', sa.Column('type', sa.String(length=20), nullable=False, server_default='expense'))
-    
-    # 2. Гарантируем, что все существующие записи получат 'expense'
-    op.execute("UPDATE transactions SET type = 'expense' WHERE type IS NULL")
 
 
 def downgrade() -> None:
