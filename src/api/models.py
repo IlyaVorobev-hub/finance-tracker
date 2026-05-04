ad132a17392a5dcdb810a8bc5c5f47998aa59a1c
Ilya Vorobev <via090105@gmail.com>
2026-05-04 21:00:16 +0300

    Fix User.created_at default value

--- src/api/models.py
+++ src/api/models.py
@@ -3,15 +3,17 @@ from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from .database import Base  # ← ИМПОРТИРУЕМ ОБЩИЙ Base из database.py
+from datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
-
+    
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    # ✅ FIX: добавляем default
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     # Отношение выносится внутрь класса
     transactions = relationship("Transaction", back_populates="user")
